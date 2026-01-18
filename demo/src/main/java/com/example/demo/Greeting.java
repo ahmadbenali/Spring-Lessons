7e638e2b8d769dccf3c764fba2ace511f4306694
ahmad ali <159959531+ahmadbenali@users.noreply.github.com>
2026-01-18 21:50:57 +0300

    Add files via upload
    Lesson 1

---
+++ demo/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.demo;
+
+// Java Record class it has only one job (hold data)
+// You can put inside it a validation like if the number lower then 10 etc..
+public record Greeting(long id, String content){}
